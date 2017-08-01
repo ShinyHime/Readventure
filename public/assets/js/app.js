00ee1128952a13d445824fe9ec9082275d5f37d1
Cassandra Claire <cassandra_villaruel@yahoo.com>
2017-07-31 21:01:57 -0500

    can search stuff

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,8 +2,38 @@
 
 $(document).foundation()
 
-function displayBookInfo() {
-  var API
-}
+$('#searchBook').on("click", function(event) {
 
-console.log("hi")
+  //Prevents form html from refreshing the page
+  event.preventDefault();
+  //store input from field in a variable
+  var title = $('#bookInput').val().trim();
+  var queryURL = "https://www.googleapis.com/books/v1/volumes?q=:" + title + "&maxResults=3";
+
+  $.ajax({
+  url: queryURL,
+  method: "GET"
+}).done(function(response) {
+
+  console.log(response);
+
+
+})
+
+  $("#bookInput").val("");
+
+})
+
+
+function displayBookInfo() {
+  var APIkey = "AIzaSyDA6jUVJy0nexi2oe7wmyLcm_EJmQ08eU0";
+  var isbn = "0545010225";
+  var queryURL = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
+
+  $.ajax({
+  url: queryURL,
+  method: "GET"
+}).done(function(response) {
+
+})
+}
