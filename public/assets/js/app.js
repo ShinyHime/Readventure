a10e3991be03da7ba1413c144c986ae6b1b9e122
Jakie Rice <jakie.rice@gmail.com>
2017-07-31 21:06:03 -0500

    trying to merge

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,46 +2,8 @@
 
 $(document).foundation()
 
-<<<<<<< HEAD
-$('#registerButton').click(function (e) {
-    e.preventDefault();
-
-    var name = $('#newUserName').val().trim();
-    var email = $('#newUserEmail').val().trim();
-    var password = $('#newUserName').val().trim();
-
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-    });
-})
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    // ...
-  } else {
-    // User is signed out.
-    // ...
-  }
-});
-
-$('#loginButton').click(function () {
-
-})
-=======
 function displayBookInfo() {
   var API
 }
 
 console.log("hi")
->>>>>>> refs/remotes/origin/master
